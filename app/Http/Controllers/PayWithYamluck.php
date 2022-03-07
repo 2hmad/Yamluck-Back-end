369bbfa254124901eb6a6d6502095c6964fbbf4e
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-03-07 03:04:43 +0200

    Pay with yamluck controller

--- app/Http/Controllers/PayWithYamluck.php
+++ app/Http/Controllers/PayWithYamluck.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Offers;
+use App\Models\Users;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PayWithYamluck extends Controller
 {
-    //
+    public function pay(Request $request)
+    {
+        $headerToken = $request->header('Authorization');
+        $checkToken = Users::where('token', $headerToken)->first();
+        if ($checkToken !== null && $headerToken !== null) {
+            $getBalance = DB::table('yamluck')->where('user_id', '=', $checkToken->id)->first();
+            $productID = Offers::where('id', $request->product_id)->first();
+            if ($getBalance >= $productID) {
+                DB::table('yamluck')->where('user_id', '=', $checkToken->id)->update([
+                    'amount' => $getBalance->amount - $productID->share_price
+                ]);
+            } else {
+                return response()->json(['alert' => 'Balance is not enough'], 404);
+            }
+        }
+    }
 }
