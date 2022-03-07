715eb30d3f447d42179261a54d2b9f1137d9c467
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-03-07 22:10:59 +0200

    Delete toJson()

--- app/Http/Controllers/OffersController.php
+++ app/Http/Controllers/OffersController.php
@@ -9,18 +9,18 @@ class OffersController extends Controller
 {
     public function randomOffers(Request $request, $limit)
     {
-        return Offers::inRandomOrder()->limit($limit)->get()->toJson();
+        return Offers::inRandomOrder()->limit($limit)->get();
     }
     public function getOffers($category_id)
     {
-        return Offers::where('category_id', $category_id)->get()->toJson();
+        return Offers::where('category_id', $category_id)->get();
     }
     public function getSubOffers($sub_category_id)
     {
-        return Offers::where('sub_category_id', $sub_category_id)->get()->toJson();
+        return Offers::where('sub_category_id', $sub_category_id)->get();
     }
     public function getSubSubOffers($sub_sub_category_id)
     {
-        return Offers::where('sub_sub_category_id', $sub_sub_category_id)->get()->toJson();
+        return Offers::where('sub_sub_category_id', $sub_sub_category_id)->get();
     }
 }
