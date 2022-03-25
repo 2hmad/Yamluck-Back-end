dabe6c2845acbd3da38c9cc8b552008a19448b63
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-03-25 17:33:31 +0200

    Delete cors package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         // \App\Http\Middleware\PreventBots::class
-        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
