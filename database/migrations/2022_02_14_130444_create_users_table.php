56b4b3c7f538b8d5715fab03a400c4b52ebcd128
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-03-25 16:54:47 +0200

    update email column

--- database/migrations/2022_02_14_130444_create_users_table.php
+++ database/migrations/2022_02_14_130444_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->text('full_name');
             $table->text('nick_name')->nullable();
-            $table->string('email', 700)->unique();
+            $table->text('email')->unique();
             $table->text('phone')->nullable();
             $table->text('country')->nullable();
             $table->text('city')->nullable();
