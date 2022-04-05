ac205864766d494873747129cc4a75e633add41c
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-04-05 19:27:48 +0200

    add repeat tables

--- app/Http/Controllers/Admin/ReportsController.php
+++ app/Http/Controllers/Admin/ReportsController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Users;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportsController extends Controller
 {
-    //
+    public function getAges()
+    {
+        $getBirthdates = Users::get('birthdate');
+        foreach ($getBirthdates as $birthdate) {
+            return Carbon::parse($birthdate->birthdate)->diff(Carbon::now())->y;
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ReportsController;
 use App\Http\Controllers\CreateInvoiceController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-});
+Route::get('/', [ReportsController::class, 'getAges']);
 Route::get('/invoice/{id}', [CreateInvoiceController::class, 'index']);
