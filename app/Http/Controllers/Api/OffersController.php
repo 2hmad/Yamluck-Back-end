6a28431d03d238fb073b08efd957221b01132db5
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-04-10 05:18:16 +0200

    update get offers

--- app/Http/Controllers/Api/OffersController.php
+++ app/Http/Controllers/Api/OffersController.php
@@ -16,19 +16,19 @@ class OffersController extends Controller
 {
     public function randomOffers(Request $request, $limit)
     {
-        return Offers::inRandomOrder()->limit($limit)->get();
+        return Offers::inRandomOrder()->with('country')->with('city')->limit($limit)->get();
     }
     public function getOffers($category_id)
     {
-        return Offers::where('category_id', $category_id)->get();
+        return Offers::where('category_id', $category_id)->with('country')->with('city')->get();
     }
     public function getSubOffers($sub_category_id)
     {
-        return Offers::where('sub_category_id', $sub_category_id)->get();
+        return Offers::where('sub_category_id', $sub_category_id)->with('country')->with('city')->get();
     }
     public function getSubSubOffers($sub_sub_category_id)
     {
-        return Offers::where('sub_sub_category_id', $sub_sub_category_id)->get();
+        return Offers::where('sub_sub_category_id', $sub_sub_category_id)->with('country')->with('city')->get();
     }
     public function addOffer(Request $request)
     {
