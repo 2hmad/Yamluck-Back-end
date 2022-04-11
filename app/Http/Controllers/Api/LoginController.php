57327c039a7d1f69edacc7a2bfb997a43fb89387
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-04-11 17:29:33 +0200

    update login notification

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -20,21 +20,16 @@ class LoginController extends Controller
         if ($checkUser) {
             if (Hash::check($request->password, $checkUser->password)) {
                 $getToken = Users::where('email', $request->email)->first(['id', 'phone', 'token', 'verified']);
-                $getNotification = Notifications::where([
-                    ['user_id', '=', $getToken->id],
-                    ['subject_en', '=', 'Login Successfully']
-                ])->first();
-                if ($getNotification == null) {
-                    Notifications::create([
-                        'user_id' => $getToken->id,
-                        'sender' => "Yammluck",
-                        'subject_en' => "Login Successfully",
-                        'subject_ar' => "تم تسجيل الدخول بنجاح",
-                        "content_en" => "Your account has been successfully logged in",
-                        "content_ar" => "لقد تمت عملية تسجيل دخول لحسابك بنجاح",
-                        "date" => Carbon::now()->toDateTimeString(),
-                    ]);
-                }
+                Notifications::create([
+                    'user_id' => $getToken->id,
+                    'sender' => "Yammluck",
+                    'subject_en' => "Login Successfully",
+                    'subject_ar' => "تم تسجيل الدخول بنجاح",
+                    "content_en" => "Your account has been successfully logged in",
+                    "content_ar" => "لقد تمت عملية تسجيل دخول لحسابك بنجاح",
+                    "date" => Carbon::now()->toDateTimeString(),
+                ]);
+
                 return $getToken;
             } else {
                 return response()->json(['alert' => 'Wrong Password'], 404);
